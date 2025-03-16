e19c5475bd161aa6d5940886e1837636d13d4afb
Deepanshu-024 <goyal.deepanshu024@gmail.com>
2025-03-16 16:33:37 +0530

    successfully linked db to frontendZ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,16 @@ import { Button } from "@/components/ui/button";
 import { supabase } from "@/lib/supabase/client";
 
 export default function Home() {
-
-const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-  const formData = new FormData(e.target as HTMLFormElement);
-  const email = formData.get("email");
-  console.log(email);
-
-  const { data, error } = await supabase.from("Users").insert({ email })
-}
-
-
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const email = formData.get("email");
+    const { data, error } = await supabase.from('Users').insert({ email })
+  }
   return (
     <div className="flex flex-col items-center justify-center mt-10">
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input type="text" name="email" placeholder="mail" />
+        <input type="text" name="email" placeholder="email" />
         <Button type="submit">Submit</Button>
       </form>
     </div>
