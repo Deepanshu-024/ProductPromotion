1a85fbeb996b1089b63f0320c05e5a62805c9fe9
Deepanshu-024 <goyal.deepanshu024@gmail.com>
2025-03-18 11:01:43 +0530

    pulled from Deepanshu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import LoginForm from "@/components/forms/loginForm";
 
-//.
+//..
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center mt-10">
