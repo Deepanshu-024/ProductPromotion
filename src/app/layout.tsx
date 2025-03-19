bb46ad5cfea19731ae848af52732e26ebbe699e5
Deepanshu-024 <goyal.deepanshu024@gmail.com>
2025-03-20 03:14:52 +0530

    added webhook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,11 @@
 import type { Metadata } from 'next'
 import {
   ClerkProvider,
-  // SignInButton,
-  // SignUpButton,
-  // SignedIn,
-  // SignedOut,
-  // UserButton,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
 } from '@clerk/nextjs'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
@@ -36,13 +36,13 @@ export default function RootLayout({
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
            <header className="flex justify-end items-center p-4 gap-4 h-16">
-            {/* <SignedOut>
+            <SignedOut>
               <SignInButton />
               <SignUpButton />
             </SignedOut>
             <SignedIn>
               <UserButton />
-            </SignedIn> */}
+            </SignedIn>
           </header>
           {children}
         </body>
